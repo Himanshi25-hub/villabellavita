e309174f3bb07fe4b586fdc4276d1010255058c9
Himanshi Dixit <59226359+Himanshi25-hub@users.noreply.github.com>
2022-03-28 09:30:19 +0530

    Add files via upload

---
+++ Index.svelte
@@ -0,0 +1,186 @@
+<div>
+  <img src="bellavita.jpg" class="-top-28 relative" alt="">
+  <div class="relative -top-20">
+  <div class="relative -top-96 ">
+    <nav class="flex  text-black relative -top-96  ">
+      <ul class="text-xS pl-80 px-28  font-semibold justify-items-end">
+        <div class="pl-42 flex pt-20 text-white space-x-20 tracking-wide text-xs relative -top-28 ">
+          <li class="cursor-pointer -my-20">HOME</li>
+  
+          <li class="cursor-pointer -my-20">THE VILLA</li>
+  
+          <li class="cursor-pointer -my-20">STAY</li>
+  
+          <li class="cursor-pointer ">
+             <div>
+                <img src="static\vbvicon.jpg" class="-top-20 relative" alt="">
+             </div>
+          </li>
+  
+          <li class="cursor-pointer -my-20 ">TOUR</li>
+  
+          <li class="cursor-pointer -my-20">GALLERY</li>
+
+          <li class="cursor-pointer -my-20">CONTACT</li>
+        </div>
+      </ul>
+    </nav>
+        <div class="h-10 w-16 mt-44 flex relative -top-52">
+            <img src="sun.svg" class="ml-36" alt="">
+            <span class="text-white px-4 py-3 ">WEATHER:</span> <span class="text-white py-3">75F</span>
+            <div class="pl-96">
+                <div class="pl-9">
+                    <button class="cursor-pointer rounded-full pl-8 border-2 rotate-90 h-9 container">
+                    <div class="w-3 h-3 mr-10 rounded-full bg-white"></div>
+                    </button>
+                </div>
+            </div>
+                <div class="flex pl-72 ">
+                    <svg xmlns="http://www.w3.org/2000/svg"  height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>
+                </div>
+                    <p class="text-white pl-2">LOCAL</p> <span class="text-white pl-1">TIME</span> <span class="text-white">:</span> <span class="pl-2 text-white">9AM</span>
+        </div>
+               <div class="pl-96 relative -top-44 ">
+                    <div class="pl-96">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
+                    </div>
+               </div>  
+        <div class="flex relative -top-40 bg-white w-3/5 h-24  ">
+              <div class="border-r-2 border-slate-300 h-24 pl-36"></div>
+              <div>
+                <p class=" mt-8 pl-6 text-xs text-slate-500">CHECK-IN</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#94a3b8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-20 -mt-5 feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                <span class="font-serif text-slate-600 pl-6 font-bold">MON,</span> <span class="font-serif text-slate-700 font-bold">DEC</span>  <span class=" font-bold text-slate-700 ">5</span>
+              </div>
+              <div class="border-r-2 border-slate-300 h-24 pl-16"></div>
+              <div>
+                <p class=" mt-8 pl-9 text-xs text-slate-500">CHECK-OUT</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#94a3b8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-28 -mt-5 feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                <span class="font-serif text-slate-600 pl-10 font-bold">TUE,</span> <span class="font-serif text-slate-700 font-bold">JAN</span>  <span class="pl-0.25 font-bold text-slate-700 ">3</span>
+              </div>
+              <div class="border-r-2 border-slate-300 h-24 pl-16"></div>
+              <div>
+                <p class=" mt-8 pl-12 text-xs text-slate-500">BEDROOMS</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#94a3b8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-28 -mt-5 feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                <span class="pl-12 font-bold text-slate-700 ">6</span>
+              </div>
+                <button class="w-48 text-white font-medium ml-12  pt-3  bg-orange-400 h-24">BOOK NOW</button>
+        </div>
+        <div class="flex relative -top-40">
+            <img src="vbv-138.jpg" class=" h-[37rem] w-1/2" alt="">
+          
+              <div class="text-3xl pt-80 pr-72 text-yellow-800 pl-16 relative -top-36 font-serif">
+                  <span>A Tropical Retreat,</span> <span>Unmatched.</span> 
+              <div>
+                  <p class="w-full text-gray-500 font-['Montserrat'] text-sm relative top-6">Leave behind the daily grind and set your sights on the <br> exceptional. Be inspired by a place that sets the new standard <br> for luxury island living. The villa itself is the epitome of <br> elegance, featuring ultra-modern design, luxurious fittings <br> and conveniences, and world-class hospitality.</p>
+              </div>
+              <button class="bg-orange-600 text-sm text-white w-36 h-10 relative top-12 "
+                  >BOOK NOW</button>
+        </div>
+      </div>
+
+    
+    
+        <div class="bg-[#51301F] w-full h-96 pt-28 pl-96 relative -top-48">
+            <div class="text-4xl text-white font-serif text-center -ml-96 pb-6">
+                <p>The Ultimate Private Experience.</p>
+            </div>
+            <div class="text-white">
+                <p>Designed for luxury travelers looking for an exceptional experience. Villa Bella Vita gives you an intimate and </p>
+                <p class="pl-2">serene environment with a luxury of space. Nested on a hilltop, affording breathtaking panaromic views of </p>
+                 <p class="pl-60"> the caribbean neighbouring islands in the distance. </p>
+            </div>
+        </div>
+        
+        <div class="relative -top-48">
+          <img src="VBV-48 (1).jpg" alt=""/>
+         </div>
+
+         <div class=" w-full h-96 pt-28 pl-96 relative -top-48">
+            <div class="text-4xl text-yellow-800 font-serif text-center -ml-96 pb-6">
+                <p>A Grand Estate.</p>
+            </div>
+            <div class="text-gray-600  ">
+                <p class="pl-8 relative -top-7">This beautifully appointed home with over 10,000 sq. ft of living space, includes 7 bedrooms,</p> 
+                <p class="pl-10 relative -top-6">8 ½ bathrooms. The seven-bedroom suites each have en-suite luxury bathrooms, six have </p>
+                <p class="pl-44 relative -top-5">king-size beds and the seventh has 2 full-size beds.</p>
+            </div>
+           
+          </div>
+
+          <div>
+            <img src="VBV-21.jpg" class=" pl-80 relative -top-64 pr-60" alt="">
+         </div>
+
+         <div class="relative -top-60 pl-96 flex ml-80" >
+           <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#A8A8A8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" pl-4 feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+           <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#A8A8A8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" pl-4 feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+           <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#A8A8A8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" pl-4 feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+           <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#A8A8A8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" pl-4 feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+           <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#A8A8A8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" pl-4 feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+         </div>
+
+         <div class="flex">
+           <div class="pt-80 pl-64 text-[#543220]">
+               <div class="text-3xl pb-8 relative -top-96 font-serif">
+                   <p>Sustainable Luxury.</p>
+               </div>
+               <div>
+               <p class="w-full h-auto  pb-6 relative -top-96 text-[#555454] font-['Montserrat'] text-sm">
+                  Villa Bella Vita was built from start to finish with the <br> environment in mind, so much so that it promises to <br> have zero carbon emissions during its lifetime. Bella Vita <br> is poweredby an A1 home system that makes Villa bella <br> Vita unique in St.Maarten. 
+               </p></div>
+               <button class="bg-orange-600 text-white px-3 py-2 relative -top-96 "
+                   >BOOK NOW</button>
+           </div>
+           <div class="w-1/2 relative -top-40 ml-44">
+               <img src="VBV-52.jpg" class="ml-4" alt="">
+           </div>
+         </div>
+       
+
+         <div class="flex bg-[#543220] h-80 relative -top-40 w-full">
+            <div>
+                <img src="vbvlogo.png" class="h-20 pl-44 relative top-16" alt="">
+
+                <p class="text-white text-xs pt-40 pl-44">&copy; 2021 Villa Bella Vita</p>
+            </div>
+
+            <div class="text-white pl-32 text-xs relative top-16 ">
+                <p>Opal Road 6, </p> 
+                <p class="relative top-2">Pelican, sint maarten </p>
+                <p class="relative top-4"> Dutch Caribbean </p>
+                <p class="relative top-6">hello@villabrllavita.com </p>
+                <p class="relative top-8"> +1 (721)543-1234 </p>
+            </div>
+
+            <div class="border-r-2 border-slate-300 h-36 pl-28 relative top-12"></div>
+
+            <div class="text-white pl-8 text-xs relative top-12 ">
+             <p>HOME</p> 
+             <p class="relative top-2">THE VILLA </p>
+             <p class="relative top-4"> STAY </p>
+             <p class="relative top-6">TOUR </p>
+             <p class="relative top-8"> GALLERY </p>
+             <p class="relative top-10"> CONTACT </p>
+            </div>
+            
+            <div>
+             
+            <div class="pl-36 flex">
+              <button class="bg-yellow-800 text-xs bg-[#654636] text-white w-72  h-8 relative top-10">
+                  Enter email to subscribe to our mailing list
+              </button>
+              <svg xmlns="http://www.w3.org/2000/svg" class="relative top-8 -rotate-90 h-10 w-10" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
+            </div>
+            <div class="pl-36 relative top-20 flex">
+             <img src="fbicon.png" class="h-8 w-5" alt="">
+             <img src="insta.png" class="h-14 w-16 relative -top-3  " alt="">
+             </div>
+         </div>
+         
+       
+       
+ </div>
+</div> 
+  </div>
+  </div>
